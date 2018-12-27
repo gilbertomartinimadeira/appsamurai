dea2161d31eecbe6810739aea4dac90721c30686
Gilberto <gilberto.madeira@outlook.com>
2018-12-27 10:16:27 -0200

    inserindo um samurai

--- SamuraiApp.Dados/SamuraiContext.cs
+++ SamuraiApp.Dados/SamuraiContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Dominio;
 
 namespace SamuraiApp.Dados
 {
     public class SamuraiContext : DbContext
-    {
+    {                    
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Frase> Frases { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
---
+++ SomeUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SamuraiApp.Dados;
+using SamuraiApp.Dominio;
+
+namespace SomeUI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Inserindo Samurai, Aguarde...");
+
+            InserirSamurai();
+
+            Console.WriteLine("Samurai inserido com sucesso!");
+
+            Console.ReadLine();
+
+        }
+
+        private static void InserirSamurai()
+        {
+            var samurai = new Samurai { Nome = "Jo" };
+
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.Add(samurai);
+
+                context.SaveChanges();
+            }
+           
+        }
+    }
+}
